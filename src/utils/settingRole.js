8121681a2cf2a5af34609c710d059a1a75cfcf3e
TingTing <ting@cakeresume.com>
2021-01-25 16:48:01 +0800

    get all of number of gods

--- src/utils/settingRole.js
+++ src/utils/settingRole.js
@@ -7,6 +7,14 @@ const HUNTER = 'ROLE_HUNTER';
 const IDIOT = 'ROLE_IDIOT';
 const VILLAGER = 'ROLE_VILLAGER';
 
+function getAllIndexes(arr, val) {
+  var indexes = [], i = -1;
+  while ((i = arr.indexOf(val, i+1)) != -1){
+      indexes.push(i + 1);
+  }
+  return indexes;
+}
+
 function setttingRole() {
   const roles = [WOLF, WOLF, WOLF, WOLF, SEER, WITCH, HUNTER, IDIOT, VILLAGER, VILLAGER, VILLAGER, VILLAGER];
 
@@ -14,7 +22,11 @@ function setttingRole() {
 
   const result = {
     roles: rolesAfterShuffle,
+    numberOfSeer: rolesAfterShuffle.findIndex(r => r === SEER) + 1,
     numberOfWitch: rolesAfterShuffle.findIndex(r => r === WITCH) + 1,
+    numberOfHunter: rolesAfterShuffle.findIndex(r => r === HUNTER) + 1,
+    numberOfIdiot: rolesAfterShuffle.findIndex(r => r === IDIOT) + 1,
+    allNumbersOfWolf: getAllIndexes(rolesAfterShuffle, WOLF),
   }
 
   console.log('rolesAfterShuffle', result);
